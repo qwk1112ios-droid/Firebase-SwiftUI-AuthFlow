cd11575d92051697d40632925442ec632ad56b90
Amel <amelsb45@gmail.com>
2026-03-29 22:21:36 -0400

    initial project setup

---
+++ Firebase-SwiftUI-AuthFlow/Firebase_SwiftUI_AuthFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  Firebase_SwiftUI_AuthFlowApp.swift
+//  Firebase-SwiftUI-AuthFlow
+//
+//  Created by Amel Sbaihi on 3/29/26.
+//
+
+import SwiftUI
+
+@main
+struct Firebase_SwiftUI_AuthFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Firebase-SwiftUI-AuthFlowTests/Firebase_SwiftUI_AuthFlowTests.swift
@@ -0,0 +1,17 @@
+//
+//  Firebase_SwiftUI_AuthFlowTests.swift
+//  Firebase-SwiftUI-AuthFlowTests
+//
+//  Created by Amel Sbaihi on 3/29/26.
+//
+
+import Testing
+@testable import Firebase_SwiftUI_AuthFlow
+
+struct Firebase_SwiftUI_AuthFlowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
